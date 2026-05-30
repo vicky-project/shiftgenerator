d68164ad06e0d2ad0249a540297136645ab6f1e9
vicky <pratamavr@gmail.com>
2026-05-31 06:37:53 +0800

    updates

--- config/config.php
+++ config/config.php
@@ -5,5 +5,9 @@ return [
   'holiday' => [
     'holiday_api_key' => env('HOLIDAY_API_KEY')
   ],
-  'back_home_route' => 'apps.index'
+  'back_home_route' => 'apps.index',
+  'hooks' => [
+    'enabled' => env('SHIFTGENERATOR_HOOK_ENABLED', false),
+    'service' => \Modules\CoreUI\Services\UIService::class,
+  ]
 ];
---
+++ resources/views/hooks/apps.blade.php
@@ -0,0 +1,6 @@
+<div class="col-4 col-md-2 mb-2">
+  <a href="{{ route('shift.web') }}" class="menu-item rounded-4 p-2 disabled" disabled="true">
+    <i class="bi bi-calendar-range"></i>
+    <span>Shift Generator</span>
+  </a>
+</div>
